f42a792d3396f970a9d33d1f72dd1f46cf1a5ad6
Crowdbotics <team@crowdbotics.com>
2020-05-19 14:59:56 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/sandman_17146/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sandman_17146;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "sandman_17146";
+    }
+}
